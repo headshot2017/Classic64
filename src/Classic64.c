fe8c601061aaf2c5339402daaee15a827f0372ca
headshot2017 <nou@jm>
2022-08-25 23:12:47 -0400

    reorder event functions

--- src/Classic64.c
+++ src/Classic64.c
@@ -259,45 +259,6 @@ static struct Model* marioModel_GetInstance(void) {
 	return &mario_model;
 }
 
-void OnChatMessage(void* obj, const cc_string* msg, int msgType)
-{
-	
-}
-
-void OnContextLost(void* obj)
-{
-	Gfx_DeleteTexture(&marioTextureID);
-	for (int i=0; i<256; i++)
-	{
-		if (marioInstances[i])
-		{
-			struct MarioInstance* obj = marioInstances[i];
-			Gfx_DeleteDynamicVb(&obj->vertexID);
-			Gfx_DeleteDynamicVb(&obj->texturedVertexID);
-#ifdef CLASSIC64_DEBUG
-			Gfx_DeleteDynamicVb(&obj->debuggerVertexID);
-#endif
-		}
-	}
-}
-
-void OnContextRecreated(void* obj)
-{
-	marioTextureID = Gfx_CreateTexture(&marioBitmap, 0, false);
-	for (int i=0; i<256; i++)
-	{
-		if (marioInstances[i])
-		{
-			struct MarioInstance* obj = marioInstances[i];
-			obj->vertexID = Gfx_CreateDynamicVb(VERTEX_FORMAT_TEXTURED, 4 * SM64_GEO_MAX_TRIANGLES);
-			obj->texturedVertexID = Gfx_CreateDynamicVb(VERTEX_FORMAT_TEXTURED, 4 * SM64_GEO_MAX_TRIANGLES);
-#ifdef CLASSIC64_DEBUG
-			obj->debuggerVertexID = Gfx_CreateDynamicVb(VERTEX_FORMAT_TEXTURED, DEBUGGER_MAX_VERTICES);
-#endif
-		}
-	}
-}
-
 // chat command
 static void OnMarioClientCmd(const cc_string* args, int argsCount)
 {
@@ -972,6 +933,47 @@ void selfMarioTick(struct ScheduledTask* task)
 	*/
 }
 
+// event callbacks
+void OnChatMessage(void* obj, const cc_string* msg, int msgType)
+{
+	
+}
+
+void OnContextLost(void* obj)
+{
+	Gfx_DeleteTexture(&marioTextureID);
+	for (int i=0; i<256; i++)
+	{
+		if (marioInstances[i])
+		{
+			struct MarioInstance* obj = marioInstances[i];
+			Gfx_DeleteDynamicVb(&obj->vertexID);
+			Gfx_DeleteDynamicVb(&obj->texturedVertexID);
+#ifdef CLASSIC64_DEBUG
+			Gfx_DeleteDynamicVb(&obj->debuggerVertexID);
+#endif
+		}
+	}
+}
+
+void OnContextRecreated(void* obj)
+{
+	marioTextureID = Gfx_CreateTexture(&marioBitmap, 0, false);
+	for (int i=0; i<256; i++)
+	{
+		if (marioInstances[i])
+		{
+			struct MarioInstance* obj = marioInstances[i];
+			obj->vertexID = Gfx_CreateDynamicVb(VERTEX_FORMAT_TEXTURED, 4 * SM64_GEO_MAX_TRIANGLES);
+			obj->texturedVertexID = Gfx_CreateDynamicVb(VERTEX_FORMAT_TEXTURED, 4 * SM64_GEO_MAX_TRIANGLES);
+#ifdef CLASSIC64_DEBUG
+			obj->debuggerVertexID = Gfx_CreateDynamicVb(VERTEX_FORMAT_TEXTURED, DEBUGGER_MAX_VERTICES);
+#endif
+		}
+	}
+}
+
+// main plugin functions
 static void Classic64_Init()
 {
 	LoadSymbolsFromGame();
